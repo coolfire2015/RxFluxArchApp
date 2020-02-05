94123d4c09732793eb372ebdfec17ddcc0ed85ff
K0170016 <k0170016@test.htsc.com.cn>
2020-02-05 09:13:34 +0800

    创建新module

--- module-epidemic/src/main/java/com/huyingbao/module/epidemic/ui/main/model/Data.kt
+++ module-epidemic/src/main/java/com/huyingbao/module/epidemic/ui/main/model/Data.kt
@@ -8,7 +8,13 @@ data class Data(
         val getEntries: List<Entry>,
         val getIndexRecommendList: List<IndexRecommend>,
         val getIndexRumorList: List<IndexRumor>,
+        /**
+         * 国家数据
+         */
         val getListByCountryTypeService: List<AreaCityStat>,
+        /**
+         * 省市数据
+         */
         val getListByCountryTypeService2: List<AreaCityStat>,
         val getPV: Int,
         val getStatisticsService: StatisticsService,
